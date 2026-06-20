957fe0a6d5558699b9c6614e417a5aab28caa7cc
Saumya Yadav <saumyayadav2603@gmail.com>
2026-06-20 19:18:51 +0530

    tried to direct our website to homepage instead of page not found

--- app.js
+++ app.js
@@ -97,11 +97,9 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.get("/", (req, res) => {
-//   res.send("Hi, I am root");
-// });
-
-
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
